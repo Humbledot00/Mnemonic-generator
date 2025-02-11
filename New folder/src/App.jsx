17dc88e911ce0c4f3cb2b8107d8ffc48807363ce
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2025-02-11 21:56:53 +0530

    Update App.jsx

--- New folder/src/App.jsx
+++ New folder/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const handleGenerate = async () => {
   try {
-    const response = await axios.post('http://127.0.0.1:5000/generate', { 
+    const response = await axios.post('https://bodhi-server.onrender.com/generate', { 
       input_text: inputText,
       model: selectedModel, 
     });
